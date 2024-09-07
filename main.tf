63f2aa6674e98017b65455d9c08ac8be70a4daec
Oliver <47428486+olimcr@users.noreply.github.com>
2024-09-07 03:33:06 +0100

    cleaning up main.tf a little fmt+ removing top comment

--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-# main.tf
-
 # Specify the provider and version
 terraform {
   backend "s3" {
@@ -44,20 +42,20 @@ resource "aws_vpc" "main_vpc" {
 
 # public subnet
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true 
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public_subnet_1"
   }
 }
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-  map_public_ip_on_launch = true 
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public_subnet_2"
